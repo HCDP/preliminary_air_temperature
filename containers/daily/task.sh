0a46793f11c5a56d5fea13c5b62f0eb5b0dd8974
jardmclan <mcleanj@hawaii.edu>
2024-04-22 12:29:29 -1000

    standardize custom date env var

--- containers/daily/task.sh
+++ containers/daily/task.sh
@@ -2,45 +2,44 @@
 echo "[task.sh] [1/5] Starting Execution."
 export TZ="HST"
 echo "It is currently $(date)."
-if [ $AGGREGATION_DATE ]; then
+if [ $CUSTOM_DATE ]; then
     echo "An aggregation date was provided by the environment."
-    echo "Aggregation date is: " $AGGREGATION_DATE
 else
-    export AGGREGATION_DATE=$(date -d "1 day ago" --iso-8601)
+    export CUSTOM_DATE=$(date -d "1 day ago" --iso-8601)
     echo "No aggregation date was provided by the environment. Defaulting to yesterday."
-    echo "Aggregation date is: " $AGGREGATION_DATE
 fi
+echo "Aggregation date is: " $CUSTOM_DATE
 
 echo "[task.sh] [2/5] Aggregating Airtemp data on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/code
 echo "---temp_agg_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py $CUSTOM_DATE
 echo "---temp_map_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $CUSTOM_DATE
 cp -rf dependencies/* /home/hawaii_climate_products_container/preliminary/air_temp/daily/dependencies
 echo "---hads_temp_parse.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/hads_temp_parse.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/hads_temp_parse.py $CUSTOM_DATE
 echo "---madis_temp_parse.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/madis_temp_parse.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/madis_temp_parse.py $CUSTOM_DATE
 echo "---air_temp_aggregate_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/air_temp_aggregate_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/air_temp_aggregate_wrapper.py $CUSTOM_DATE
 
 echo "[task.sh] [3/5] Mapping Airtemp data on the daily timeframe."
 cd /home/hawaii_climate_products_container/
 
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/
 echo "---update_predictor_table.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/update_predictor_table.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/update_predictor_table.py $CUSTOM_DATE
 echo "---county_map_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/county_map_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/county_map_wrapper.py $CUSTOM_DATE
 echo "---meta_data_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/meta_data_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/meta_data_wrapper.py $CUSTOM_DATE
 echo "---state_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/state_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/state_wrapper.py $CUSTOM_DATE
 
 echo "[task.sh] [4/5] Preparing to upload data."
 cd /sync
-python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $AGGREGATION_DATE
+python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $CUSTOM_DATE
 python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json
 python3 add_auth_info_to_config.py config.json
 
--- containers/monthly/task.sh
+++ containers/monthly/task.sh
@@ -2,35 +2,34 @@
 echo "[task.sh] [1/4] Starting Execution."
 export TZ="HST"
 echo "It is currently $(date)."
-if [ $AGGREGATION_DATE ]; then
+if [ $CUSTOM_DATE ]; then
     echo "An aggregation date was provided by the environment."
-    echo "Aggregation date is: " $AGGREGATION_DATE
 else
-    export AGGREGATION_DATE=$(date -d "1 month ago" --iso-8601)
+    export CUSTOM_DATE=$(date -d "1 month ago" --iso-8601)
     echo "No aggregation date was provided by the environment. Defaulting to last month."
-    echo "Aggregation date is: " $AGGREGATION_DATE
 fi
+echo "Aggregation date is: " $CUSTOM_DATE
 
 echo "[task.sh] [2/4] Mapping Airtemp data on the monthly timeframe."
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/
 echo "---monthly_map_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_map_wget.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_map_wget.py $CUSTOM_DATE
 echo "---update_monthly_predictor.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/update_monthly_predictor.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/update_monthly_predictor.py $CUSTOM_DATE
 echo "---monthly_map_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_map_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_map_wrapper.py $CUSTOM_DATE
 echo "---monthly_meta_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_meta_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_meta_wrapper.py $CUSTOM_DATE
 echo "---monthly_state_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_state_wrapper.py $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_state_wrapper.py $CUSTOM_DATE
 echo "---monthly_stn_data.py--- on Tmax"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmax $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmax $CUSTOM_DATE
 echo "---monthly_stn_data.py--- on Tmin"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmin $AGGREGATION_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmin $CUSTOM_DATE
 echo "[task.sh] [3/4] Preparing to upload data."
 cd /sync
-python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $AGGREGATION_DATE
-python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json $AGGREGATION_DATE
+python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $CUSTOM_DATE
+python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json $CUSTOM_DATE
 python3 add_auth_info_to_config.py config.json
 
 echo "[task.sh] [4/4] Uploading data."
