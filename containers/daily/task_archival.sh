94802981981a160434f982cf1d64aaf2bef93702
jardmclan <mcleanj@hawaii.edu>
2026-02-11 17:40:25 -1000

    merge changes, update deps, update run script, add daily env, change container folder structure to /workspace, collapse processed_data uploads in config

--- containers/daily/task_archival.sh
+++ containers/daily/task_archival.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 echo "[task.sh] [2/5] Aggregating Airtemp data on the daily timeframe."
-cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/code
+
 echo "---temp_agg_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/temp_agg_wget.py $CUSTOM_DATE
 echo "---temp_map_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $CUSTOM_DATE
-cp -rf dependencies/* /home/hawaii_climate_products_container/preliminary/air_temp/daily/dependencies
+python3 -W ignore /workspace/code/temp_map_wget.py $CUSTOM_DATE
+cp -rf dependencies/* /workspace/dependencies
 
 echo "[task.sh] [3/5] Mapping Airtemp data on the daily timeframe."
-cd /home/hawaii_climate_products_container/
 
-cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/
 echo "---update_predictor_table.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/update_predictor_table.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/update_predictor_table.py $CUSTOM_DATE
 echo "---county_map_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/county_map_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/county_map_wrapper.py $CUSTOM_DATE
 echo "---meta_data_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/meta_data_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/meta_data_wrapper.py $CUSTOM_DATE
 echo "---state_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/state_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/state_wrapper.py $CUSTOM_DATE
 
 echo "[task.sh] [4/5] Preparing to upload data."
 cd /sync
--- containers/daily/task_current.sh
+++ containers/daily/task_current.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 echo "[task.sh] [2/5] Aggregating Airtemp data on the daily timeframe."
-cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/code
+
 echo "---temp_agg_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/temp_agg_wget.py $CUSTOM_DATE
 echo "---temp_map_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $CUSTOM_DATE
-cp -rf dependencies/* /home/hawaii_climate_products_container/preliminary/air_temp/daily/dependencies
+python3 -W ignore /workspace/code/temp_map_wget.py $CUSTOM_DATE
+cp -rf dependencies/* /workspace/dependencies
 echo "---hads_temp_parse.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/hads_temp_parse.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/hads_temp_parse.py $CUSTOM_DATE
 echo "---madis_temp_parse.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/madis_temp_parse.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/madis_temp_parse.py $CUSTOM_DATE
+echo "---himeso_temp_parse.py---"
+python3 -W ignore /workspace/code/himeso_temp_parse.py $CUSTOM_DATE
 echo "---air_temp_aggregate_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/air_temp_aggregate_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/air_temp_aggregate_wrapper.py $CUSTOM_DATE
 
 echo "[task.sh] [3/5] Mapping Airtemp data on the daily timeframe."
-cd /home/hawaii_climate_products_container/
 
-cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/
 echo "---update_predictor_table.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/update_predictor_table.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/update_predictor_table.py $CUSTOM_DATE
 echo "---county_map_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/county_map_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/county_map_wrapper.py $CUSTOM_DATE
 echo "---meta_data_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/meta_data_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/meta_data_wrapper.py $CUSTOM_DATE
 echo "---state_wrapper.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/state_wrapper.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/state_wrapper.py $CUSTOM_DATE
 
 echo "[task.sh] [4/5] Preparing to upload data."
 cd /sync
